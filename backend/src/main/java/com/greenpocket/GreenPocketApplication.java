c7cfc4d2184dd919e76f94e7d7fb4638b1269160
junsoo0719 <junsoo0719@gmail.com>
2026-09-01 17:48:31 +0900

    chore: 그린포켓 모노레포 및 백엔드 초기 구조 구성

---
+++ backend/src/main/java/com/greenpocket/GreenPocketApplication.java
@@ -0,0 +1,13 @@
+package com.greenpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenPocketApplication.class, args);
+	}
+
+}
