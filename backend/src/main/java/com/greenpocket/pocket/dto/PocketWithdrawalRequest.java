f83ea9ca0ad6444b8e8b4b382bcfec4dfcc0feab
junsoo0719 <junsoo0719@gmail.com>
2026-09-04 19:31:50 +0900

    fix: 출금 금액 Swagger 타입 수정

--- backend/src/main/java/com/greenpocket/pocket/dto/PocketWithdrawalRequest.java
+++ backend/src/main/java/com/greenpocket/pocket/dto/PocketWithdrawalRequest.java
@@ -5,7 +5,7 @@ import tools.jackson.databind.JsonNode;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PocketWithdrawalRequest(
-	@Schema(type = "integer", format = "int64", example = "30000", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(implementation = Long.class, example = "30000", requiredMode = Schema.RequiredMode.REQUIRED)
 	JsonNode amount,
 	@Schema(example = "3", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long accountId
